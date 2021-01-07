aad967ccc3993b318aaa20a4061788a939357a1f
BuildTools <unconfigured@null.spigotmc.org>
2021-01-07 13:25:19 +0300

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
 def main():
-   print("Keep it logically awesome.")
-
+  import random
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
+  last = len(quotes)-1
+  rnd = random.randint(0, last)
 
-  print(quotes[0])
+  print(quotes[rnd])
 
 if __name__== "__main__":
-  primary()
+  main()
